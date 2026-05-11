dfb7694b2e221c88fccd6d5a2175241d0ec378e5
Lê Minh Khôi <lewingkai70a@gmail.com>
2026-05-11 16:06:54 +0700

    Add: remove-duplicates-from-sorted-array - submission-0

---
+++ Data Structures & Algorithms/remove-duplicates-from-sorted-array/submission-0.js
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    removeDuplicates(nums) {
+        let i = 1
+
+        while (i < nums.length) {
+            if (nums[i] === nums[i - 1]) {
+                nums.splice(i, 1)
+            } else {
+                i++
+            }
+        }
+
+        return nums.length
+    }
+}
