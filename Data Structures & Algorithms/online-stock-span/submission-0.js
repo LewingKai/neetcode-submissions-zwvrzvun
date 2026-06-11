8a03907506a09623854f02a3f8e1829040f46120
Lê Minh Khôi <lewingkai70a@gmail.com>
2026-06-11 20:23:24 +0700

    Add: online-stock-span - submission-0

---
+++ Data Structures & Algorithms/online-stock-span/submission-0.js
@@ -0,0 +1,21 @@
+class StockSpanner {
+    constructor() {
+        this.stack = []; // pair: [price, span]
+    }
+
+    /**
+     * @param {number} price
+     * @return {number}
+     */
+    next(price) {
+        let span = 1;
+        while (
+            this.stack.length &&
+            this.stack[this.stack.length - 1][0] <= price
+        ) {
+            span += this.stack.pop()[1];
+        }
+        this.stack.push([price, span]);
+        return span;
+    }
+}
